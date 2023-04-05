a52db19b76f6cbc2bd9d6f20f76f101ac5c0e2bc
Luciano E. Yacob <yeicob14@gmail.com>
2023-04-05 13:58:49 -0300

    EJ9 añadido: Verificar si el numero ingresado es 0

--- EJ_9.c
+++ EJ_9.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include "TP1_Recursividad.h"
 
+bool esCero(int, int); 
+
 void main(){
     // divisiblePor7(999999);
-    // divisiblePor7(32291);
+    divisiblePor7(32291);
     // divisiblePor7(110);
     // divisiblePor7(7863);
     // divisiblePor7(-32291);
+    divisiblePor7(7);
     divisiblePor7(0);
 }
 
 // 78756 -> 6*2 = 12 -> 7875 - 12 = 7863 -> repito
 void divisiblePor7(int numero){
+    // static i = 1, se ejecuta una unica vez en la funcion donde esta definida y su valor se mantiene hasta salir de dicho programa, asi q cuando mas abajo en el codigo cambio i a 0, la variable quedara en 0 hasta finalizar la ejecucion. Esto me permite verificar si el primer valor ingresado en la llamada a la funcion es 0. 
+    static int i = 1;
+    if (esCero(numero, i)){
+        printf("El numero ingresado debe ser mayor a 0\n");
+        return;
+    }
     if (numero > 99999) {
-        printf("El numero ingresado debe ser mayor a 0 y menor a 100.000\n");
+        printf("El numero ingresado debe ser menor a 100.000\n");
         return;
     }
     if (numero < 70){
@@ -22,6 +31,7 @@ void divisiblePor7(int numero){
         return;
     }
 
+    i = 0;
     // Obtengo el ultimo digito
     int ultimoDigito = numero % 10;
     // Multiplico el ultimo digito por 2
@@ -33,4 +43,13 @@ void divisiblePor7(int numero){
 
     // y repito hasta que resultado sea < a 70
     divisiblePor7(resultado);
+    i = 1;
+}
+
+bool esCero(int num, int i) {
+    bool cero = false;
+    if (num == 0 && i == 1) cero = true;
+    else cero = false;
+
+    return cero;
 }
